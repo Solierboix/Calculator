6bec31cc28426da3056ed7302f8b1af878e1e4b9
walczpio <piotr.walczak@tieto.com>
2017-04-10 16:36:28 +0200

    hagaggaga

--- app/src/main/java/com/example/solierboix/calculator/fragment/TabFragment1.java
+++ app/src/main/java/com/example/solierboix/calculator/fragment/TabFragment1.java
@@ -8,10 +8,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.solierboix.calculator.R;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Solierboix on 18.03.2017.
@@ -19,23 +23,29 @@ import java.util.List;
 
 public class TabFragment1 extends Fragment {
 
-    private ListView playersList;
+
+    private View view = null;
     private ArrayAdapter playerAdapter;
     private String[] items;
+    @BindView(R.id.ButterKnifeTextView) TextView ButterText;
+    @BindView(R.id.saveListView) ListView playersList;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.tab_fragment1, container, false);
+        view = inflater.inflate(R.layout.tab_fragment1, container, false);
+        ButterKnife.bind(this, view);
+        setAdapterToListView();
 
-        items = getActivity().getResources().getStringArray(R.array.players_array);
-        playersList = (ListView)view.findViewById(R.id.saveListView);
-
-        playerAdapter = new ArrayAdapter<String>(view.getContext(), R.layout.tab_fragment1, items);
-
-        playersList.setAdapter(playerAdapter);
+        ButterText.setText("TestTest");
         return view;
 
 
+    }
+
+    private void setAdapterToListView() {
+        items = getActivity().getResources().getStringArray(R.array.players_array);
+        playerAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, items);
+        playersList.setAdapter(playerAdapter);
     }
 }
